f9ac27f39b38bd8812e4e33d635493b4eaf915a9
Hari Kishan Reddy <kanishkablack@users.noreply.github.com>
2026-02-12 13:44:44 +0530

    version dump

--- lib/rust_req/native.ex
+++ lib/rust_req/native.ex
@@ -8,14 +8,11 @@ defmodule RustReq.Native do
     crate: "rust_req_nif",
     base_url:
       "https://github.com/kanishkablack/rust_req/releases/download/v#{version}/",
-          force_build: System.get_env("RUST_REQ_BUILD") in ["1", "true"],
-              version: version,downl/v#{version}/",
     force_build: System.get_env("RUST_REQ_BUILD") in ["1", "true"],
     version: version,
     targets: ~w(
       aarch64-apple-darwin
       x86_64-apple-darwin
-      aarch64-unknown-linux-gnu
       x86_64-unknown-linux-gnu
       x86_64-unknown-linux-musl
       arm-unknown-linux-gnueabihf
